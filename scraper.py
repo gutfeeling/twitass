4774fc89b22c1bb40e6f7cb449cd3f4a5d2f0785
Dibya Chakravorty <dibyachakravorty@gmail.com>
2016-08-02 06:06:29 +0200

    using Selenium to simulate scrolling

--- scraper.py
+++ scraper.py
@@ -1,82 +1,50 @@
 import time
-import json
-import warnings
+import os
 
 try:
     from urllib.parse import urlencode
 except ImportError:
     from urllib import urlencode
 
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-warnings.filterwarnings('ignore')
 
 class AdvancedSearchScraper(object):
 
-    def __init__(self, all_of_these_words, limit = 100):
+    def __init__(self, all_of_these_words, scroll_limit = 10):
         self.all_of_these_words = all_of_these_words
-        self.driver = webdriver.PhantomJS()
-        if limit:
-            self.limit = limit
+        self.driver = webdriver.Chrome(os.environ["CHROMEDRIVER_PATH"])
+        if scroll_limit:
+            self.scroll_limit = scroll_limit
         else:
-            self.limit = float("inf")
+            self.scroll_limit = float("inf")
 
     def first_page_url(self):
         query_dict = {"src" : "typd",
                       "q" : self.all_of_these_words,
-                      "f" : "tweets",
                       }
         query_string = urlencode(query_dict)
         url = "https://twitter.com/search?" + query_string
         return url
 
-    def ajax_call_url(self, oldest_tweet_id, newest_tweet_id):
-        query_dict = {"src" : "typd",
-                      "q" : self.all_of_these_words,
-                      "f" : "tweets",
-                      "vertical" : "default",
-                      "include_available_features" : 1,
-                      "include_entities" : 1,
-                      "reset_error_state" : "false",
-                      "max_position" : "TWEET-%s-%s" %(oldest_tweet_id, newest_tweet_id),
-                      }
-        query_string = urlencode(query_dict)
-        url = "https://twitter.com/i/search/timeline?" + query_string
-        return url
-
     def scrape(self):
-        self.tweets = []
 
         #first-page
         self.driver.get(self.first_page_url())
-        self.tweets+=self.get_tweets_from_html(self.driver.page_source)
-        self.driver.close()
+
+        scroll_num = 0
 
         #ajax
-        if len(self.tweets)>0:
+        while scroll_num < self.scroll_limit:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(5)
+            scroll_num +=1
 
-            newest_tweet_id = self.tweets[0]['tweet_id']
-            oldest_tweet_id = self.tweets[-1]['tweet_id']
-            has_more_items = True
+        page_source = self.driver.page_source
+        self.driver.quit()
 
-            while len(self.tweets) < self.limit and has_more_items:
-
-                time.sleep(1)
-
-                response = requests.get(self.ajax_call_url(oldest_tweet_id, newest_tweet_id),
-                                        verify = False)
-                json_data = json.loads(response.text)
-                self.tweets+=self.get_tweets_from_html(json_data["items_html"])
-                oldest_tweet_id = self.tweets[-1]['tweet_id']
-                has_more_items = json_data["has_more_items"]
-
-        return self.tweets
-
-
-
-
+        return self.get_tweets_from_html(page_source)
 
     def get_tweets_from_html(self, html_doc):
         tweetlist = []
